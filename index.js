2bab3db4c8b8c35e92781c1bd37fa57077752069
Ash Rhazaly <ashleighrhazaly@gmail.com>
2017-02-21 23:39:48 +0800

    Delete unnecessary files

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ var _randomNumber = Math.floor(Math.random() * (100 - 0)) + 0;
 
 function checkNumber () {
   var guess = document.getElementById("guess").value;
-  var lower_bound = document.getElementById("left-box");
-  var upper_bound = document.getElementById("right-box");
   if (guess == _randomNumber) {
     alert('WELL DONE YOU GOT IT!');
   }
-  else if (guess >)
+  else if ( guess > _randomNumber ) {
+    document.getElementById("right-number").innerHTML = guess;
+  }
+  else if (guess < _randomNumber ){
+    document.getElementById("left-number").innerHTML = guess;
+  }
 }
