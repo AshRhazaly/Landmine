313c11a50533bd9d47dc7fc7714968efb40c55b9
Ash Rhazaly <ashleighrhazaly@gmail.com>
2017-02-21 23:28:32 +0800

    Create files and framework

---
+++ index.js
@@ -0,0 +1,12 @@
+var _randomNumber = Math.floor(Math.random() * (100 - 0)) + 0;
+
+
+function checkNumber () {
+  var guess = document.getElementById("guess").value;
+  var lower_bound = document.getElementById("left-box");
+  var upper_bound = document.getElementById("right-box");
+  if (guess == _randomNumber) {
+    alert('WELL DONE YOU GOT IT!');
+  }
+  else if (guess >)
+}
